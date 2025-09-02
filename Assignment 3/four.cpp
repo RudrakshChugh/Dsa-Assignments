63b840762b1c4091316c0e9efa4ce107d9962bbc
RudrakshChugh <rudrakshchugh005@gmail.com>
2025-09-02 10:38:03 +0530

    .

--- Assignment 3/four.cpp
+++ Assignment 3/four.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
 #include <stack>
 #include <string>
-#include <cmath>   // for pow()
+#include <cmath>
 using namespace std;
 
 int evaluatePostfix(string expr) {
     stack<int> st;
 
     for (char ch : expr) {
-        // If operand (digit)
         if (isdigit(ch)) {
-            st.push(ch - '0');   // convert char to int
+            st.push(ch - '0');  
         }
-        // If operator
         else {
             int val2 = st.top(); st.pop();
             int val1 = st.top(); st.pop();
@@ -23,15 +21,16 @@ int evaluatePostfix(string expr) {
                 case '*': st.push(val1 * val2); break;
                 case '/': st.push(val1 / val2); break;
                 case '^': st.push(pow(val1, val2)); break;
+                default: cout << "Invalid operator: " << ch << endl; return -1;
             }
         }
     }
-    return st.top();  // final result
+    return st.top();
 }
 
 int main() {
     string expr;
-    cout << "Enter Postfix Expression: ";
+    cout << "Enter Postfix Expression";
     cin >> expr;
 
     cout << "Result = " << evaluatePostfix(expr) << endl;
