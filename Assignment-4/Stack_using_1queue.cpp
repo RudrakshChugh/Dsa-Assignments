9ff076f1ca8af02922932188300a22d0d07120bd
RudrakshChugh <rudrakshchugh005@gmail.com>
2025-11-04 10:05:34 +0530

    assignment-4

---
+++ Assignment-4/Stack_using_1queue.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Stack {
+    queue<int> q;
+public:
+    void push(int x) {
+        q.push(x);
+        for (int i = 0; i < q.size() - 1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    void pop() {
+        if (!q.empty()) q.pop();
+    }
+    int top() {
+        return q.empty() ? -1 : q.front();
+    }
+    bool empty() {
+        return q.empty();
+    }
+};
+
+int main() {
+    Stack s;
+    s.push(5);
+    s.push(15);
+    s.push(25);
+    cout << s.top() << endl;
+    s.pop();
+    cout << s.top() << endl;
+}
